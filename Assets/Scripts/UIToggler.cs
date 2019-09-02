c4e93c3dcb00189b50d6b5f349577b4d3d9b15ea
mhlongley88 <mhlongley88@gmail.com>
2019-09-02 00:09:08 -0700

    UI on/off toggler for capturing footage

---
+++ Assets/Scripts/UIToggler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggler : MonoBehaviour
+{
+    public GameObject UIElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            UIElements.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            UIElements.SetActive(true);
+        }
+
+    }
+}
