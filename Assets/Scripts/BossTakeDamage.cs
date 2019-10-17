b670443449444fa3a1bd0bcd423ebe9dc1e25b01
Elvis L <elvis.leite.9@gmail.com>
2019-10-17 15:22:00 -0700

    added hit fx when player bullets hit boss;

--- Assets/Scripts/BossTakeDamage.cs
+++ Assets/Scripts/BossTakeDamage.cs
@@ -7,12 +7,14 @@ public class BossTakeDamage : MonoBehaviour
 {
     public LayerMask bulletMask;
     public UnityEvent OnBossTakeDamage = new UnityEvent();
+    public GameObject hitFx;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(GameUtils.LayerMaskContains(collision.gameObject.layer, bulletMask))
         {
             OnBossTakeDamage.Invoke();
+            Instantiate(hitFx, collision.contacts[0].point, gameObject.transform.rotation);
         }
     }
 }
