755ed1bc3fcb8f1bc55bd30774469b851ad893c5
Elvis L <elvis.leite.9@gmail.com>
2019-11-25 20:56:15 -0800

    logging keys for unlock system

--- Assets/Scripts/UnlockSystem.cs
+++ Assets/Scripts/UnlockSystem.cs
@@ -29,20 +29,44 @@ public class UnlockSystem : MonoBehaviour
     {
         matchesCompleted = PlayerPrefs.GetInt("BattlesCompleted", 0);
 
-        Debug.Log("Getting matches completed at initialization: " + matchesCompleted);
+        //Debug.Log("Getting matches completed at initialization: " + matchesCompleted);
     }
+	
+	void Update()
+	{
+#if UNITY_EDITOR
+		if(Input.GetKeyDown(KeyCode.I))
+		{
+			Debug.Log("Current Matches Completed: " + matchesCompleted);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.O))
+		{
+			matchesCompleted = 0;
+			PlayerPrefs.SetInt("BattlesCompleted", matchesCompleted);
+			PlayerPrefs.Save();
+		
+			Debug.Log("Zeroed Matches Completed: " + matchesCompleted);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			SaveMatchesCompleted();
+			Debug.Log("Added match: " + matchesCompleted);
+		}
+#endif
+	}
 
     public void SaveMatchesCompleted()
     {
         matchesCompleted++;
         PlayerPrefs.SetInt("BattlesCompleted", matchesCompleted);
-
         PlayerPrefs.Save();
 
         onBattlesCompletedChange.Invoke();
 
-        Debug.Log("One more match completed!");
-        Debug.Log("Matches Completed: " + matchesCompleted);
+        //Debug.Log("One more match completed!");
+        //Debug.Log("Matches Completed: " + matchesCompleted);
     }
 
     public int GetMatchesCompleted()
