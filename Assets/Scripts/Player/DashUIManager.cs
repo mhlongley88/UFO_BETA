ff62e1afd6d5d8cf1b624c4f08d61a86c1a6c873
Elvis L <elvis.leite.9@gmail.com>
2019-12-06 10:59:46 -0800

    no message

---
+++ Assets/Scripts/Player/DashUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashUIManager : MonoBehaviour
+{
+    public Image fill;
+
+    private void OnEnable()
+    {
+        fill.fillAmount = 1.0f;
+    }
+
+    public void SetPercentage(float percentage)
+    {
+        float r = percentage / 100.0f;
+        fill.fillAmount = r;
+    }
+}
