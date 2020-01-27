4c34e9b93c826ab9a5bb5a840fecb30a7d496f9b
mhlongley88 <mhlongley88@gmail.com>
2020-01-27 09:55:07 -0800

    sound stuff for MXC

---
+++ Assets/Scripts/MusicIntroToLoop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicIntroToLoop : MonoBehaviour
+{
+    public AudioSource musicIntro;
+    public AudioSource musicLoop;
+    void Start()
+    {
+        musicIntro.Play();
+        musicLoop.PlayDelayed(musicIntro.clip.length);
+    }
+
+}
