45dd0bcc2fd9271bb28226c4b41029760bccdd4a
elvismd <elvis.leite.9@gmail.com>
2020-02-18 00:22:46 -0300

    added keys to test unlock vfx

--- Assets/Scripts/UnlockNotification.cs
+++ Assets/Scripts/UnlockNotification.cs
@@ -7,52 +7,78 @@ using DG.Tweening;
 public class UnlockNotification : MonoBehaviour
 {
     public static UnlockNotification instance;
-    public RectTransform canvasObject;
-    public TextMeshProUGUI label;
-    public float notificationDuration = 3.0f;
+    //public RectTransform canvasObject;
+    //public TextMeshProUGUI label;
+    //public float notificationDuration = 3.0f;
+    //float canvasInitialLocalY;
+    //bool currentlyShowing;
 
-    float canvasInitialLocalY;
-
-    bool currentlyShowing;
+    public GameObject characterUnlockedVfx;
+    public GameObject levelUnlockedVfx;
 
     // Start is called before the first frame update
     void Awake()
     {
-        if (instance != null && instance != this)
-            Destroy(gameObject);
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        instance = this;
+
+        //if (instance != null && instance != this)
+        //{
+        //    //Destroy(gameObject);
+        //}
+        //else
+        //{
+        //    instance = this;
+        //    // DontDestroyOnLoad(gameObject);
+        //}
     }
 
-    private void Start()
-    {
-        //canvasObject.localPosition += Vector3.down * canvasObject.sizeDelta.y;
-        canvasInitialLocalY = canvasObject.localPosition.y;
-    }
+    //private void Start()
+    //{
+    //    //canvasObject.localPosition += Vector3.down * canvasObject.sizeDelta.y;
+    //    canvasInitialLocalY = canvasObject.localPosition.y;
+    //}
 
     private void Update()
     {
 #if UNITY_EDITOR
         //test
         if (Input.GetKeyDown(KeyCode.J))
-            Show("Hey! test!");
+            SignalUnlockCharacter();
+
+        if (Input.GetKeyDown(KeyCode.K))
+            SignalUnlockLevel();
 #endif
     }
 
-    public void Show(string text)
+    public void SignalUnlockCharacter()
     {
-        if (currentlyShowing) return;
-
-        currentlyShowing = true;
-        label.text = text;
+        characterUnlockedVfx.SetActive(true);
 
         Sequence seq = DOTween.Sequence();
-        seq.Append(canvasObject.DOLocalMoveY(canvasObject.localPosition.y + canvasObject.sizeDelta.y, 0.5f));
-        seq.AppendInterval(notificationDuration);
-        seq.Append(canvasObject.DOLocalMoveY(canvasInitialLocalY, 0.5f));
-        seq.AppendCallback(() => currentlyShowing = false);
+        seq.AppendInterval(3.0f);
+        seq.AppendCallback(() => characterUnlockedVfx.SetActive(false));
     }
+
+    public void SignalUnlockLevel()
+    {
+        levelUnlockedVfx.SetActive(true);
+
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(3.0f);
+        seq.AppendCallback(() => levelUnlockedVfx.SetActive(false));
+    }
+
+    //public void Show(string text)
+    //{
+    //    if (currentlyShowing) return;
+
+    //    currentlyShowing = true;
+    //    label.text = text;
+
+    //    Sequence seq = DOTween.Sequence();
+    //    seq.Append(canvasObject.DOLocalMoveY(canvasObject.localPosition.y + canvasObject.sizeDelta.y, 0.5f));
+    //    seq.AppendInterval(notificationDuration);
+    //    seq.Append(canvasObject.DOLocalMoveY(canvasInitialLocalY, 0.5f));
+    //    seq.AppendCallback(() => currentlyShowing = false);
+    //}
 }
