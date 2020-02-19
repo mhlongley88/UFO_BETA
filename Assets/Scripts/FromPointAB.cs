cbbb83f26d72dd01437c0f500be15feacd19b348
elvismd <elvis.leite.9@gmail.com>
2020-02-19 19:08:17 -0300

    added vfx for unlock

---
+++ Assets/Scripts/FromPointAB.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromPointAB : MonoBehaviour
+{
+    public Transform ina;
+    public Transform inb;
+    public Vector3 dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(ina.position, inb.position);
+
+        dist = inb.position - ina.position;
+    }
+}
---
+++ Assets/Scripts/FromPointABBasedOnDist.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromPointABBasedOnDist : MonoBehaviour
+{
+    public FromPointAB fpAB;
+    public Transform a;
+    public Transform b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 ap = a.position;
+        ap += fpAB.dist;
+        b.position = ap;
+    }
+}
