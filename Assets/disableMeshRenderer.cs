089f4398505e9b87dfcab2093edcddb07d71e425
mhlongley88 <mhlongley88@gmail.com>
2020-04-11 23:06:45 -0700

    more polish coming into Hawaii

---
+++ Assets/disableMeshRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableMeshRenderer : MonoBehaviour
+{
+
+    public MeshRenderer myMeshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMeshRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
