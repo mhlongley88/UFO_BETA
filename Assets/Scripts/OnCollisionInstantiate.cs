8e846141cfa1c3a55d49bc784c538fb7b8ac5fe4
mhlongley88 <mhlongley88@gmail.com>
2020-04-12 19:51:17 -0700

    Skyfall in Hawaii.

---
+++ Assets/Scripts/OnCollisionInstantiate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionInstantiate : MonoBehaviour
+{
+    public GameObject ExplosionPfX;
+    private bool spawnedOnce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedOnce = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (spawnedOnce == true)
+        {
+            Instantiate(ExplosionPfX, gameObject.transform.position, Quaternion.identity);
+            spawnedOnce = false;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            //yield return new WaitForSeconds(2);
+            // Destroy(gameObject);
+        }
+    }
+
+}
