110f94d5acac0a64ded086b59b05ac5230a8fa34
mhlongley88 <mhlongley88@gmail.com>
2020-04-12 23:04:26 -0700

    Hawaii Boss fight sections almost done!!

---
+++ Assets/Scripts/SwitchAnims.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnims : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject characterINTRO;
+    public GameObject characterMID;
+
+    void OnEnable()
+    {
+        characterINTRO.SetActive(false);
+        characterMID.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
