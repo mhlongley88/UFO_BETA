020eb7b928768f70a00955cf402a28825daafba0
mhlongley88 <mhlongley88@gmail.com>
2020-04-19 17:55:32 -0700

    Sounds for HI Boss!!

---
+++ Assets/Scripts/SwitcherooReverse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitcherooReverse : MonoBehaviour
+{
+    public GameObject watchedObject;
+    private bool switchIt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchIt = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.gameObject.SetActive(true);
+
+        if (switchIt == true)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if(watchedObject == true)
+        {
+            switchIt = true;
+        }
+    }
+}
