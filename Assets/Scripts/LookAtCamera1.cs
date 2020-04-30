def5b0f6ac48283f8c199fa561389ffc23af53ab
mhlongley88 <mhlongley88@gmail.com>
2020-04-29 20:08:05 -0700

    Intro cutscene coming together!

---
+++ Assets/Scripts/LookAtCamera1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera1 : MonoBehaviour
+{
+    private GameObject myCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(myCam.transform);
+        
+    }
+}
