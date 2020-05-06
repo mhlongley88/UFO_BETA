60417751970516099329310e9d1fa7e54bc3788b
elvismd <elvis.leite.9@gmail.com>
2020-05-06 11:00:14 -0300

    unlock from progression test

--- Assets/Scripts/Player/LevelUnlockFromProgression.cs
+++ Assets/Scripts/Player/LevelUnlockFromProgression.cs
@@ -8,7 +8,7 @@ public class LevelUnlockFromProgression : MonoBehaviour
 
     public bool allowUnlockFromProgression = false;
     public int unlocksWhatLevel = -1;
-    static string keyLevelUnlock = "UFO_LevelUnlockedProgression";
+    static string keyLevelUnlock = "_UFO_LevelUnlockedProgression_";
     //int unlockedState;
    // public int UnlockedState => unlockedState;
 
