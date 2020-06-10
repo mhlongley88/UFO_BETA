ef05a7f22848859b8d612655fd7c1880ce2db917
mhlongley88 <mhlongley88@gmail.com>
2020-06-09 22:47:41 -0700

    Random BG and Earth for End Screen.

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+
+    public int maxRandomRange;
+
+    private int randNum;
+
+    public GameObject[] backgrounds;
+    public GameObject[] earths;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        randNum = Random.Range(0, maxRandomRange);
+        backgrounds[randNum].SetActive(true);
+        earths[randNum].SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        randNum = Random.Range(0, maxRandomRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
