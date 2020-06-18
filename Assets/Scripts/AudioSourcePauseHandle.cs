4bae94f5fcae6bc952f9b5acb3f972350b04d5ae
elvismd <elvis.leite.9@gmail.com>
2020-06-18 10:48:52 -0300

    Pause Functionality - Pausing/Unpausing does not Pause/Unpause SFX for Boss.

---
+++ Assets/Scripts/AudioSourcePauseHandle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcePauseHandle : MonoBehaviour
+{
+    AudioSource[] sources;
+    bool valuePaused;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sources = GetComponentsInChildren<AudioSource>(true);
+
+        valuePaused = GameManager.Instance.paused;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.paused != valuePaused)
+        {
+            valuePaused = GameManager.Instance.paused;
+
+            for (int i = 0; i < sources.Length; i++)
+            {
+                var src = sources[i];
+
+                if (valuePaused)
+                    src.Pause();
+                else
+                    src.Play();
+            }
+        }
+    }
+}
