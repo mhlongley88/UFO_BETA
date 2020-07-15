2cea2f5f660078f0980f5ded49fd2ec0cb9097fe
elvismd <elvis.leite.9@gmail.com>
2020-07-15 15:01:06 -0300

    higher camera target weight for player when in SP mode

--- Assets/Scripts/AddToTargetGroup.cs
+++ Assets/Scripts/AddToTargetGroup.cs
@@ -5,22 +5,34 @@ using Cinemachine;
 
 public class AddToTargetGroup : MonoBehaviour
 {
-
+    const float normalWeight = 1.0f;
+    const float preferredWeight = 3.0f;
     private CinemachineTargetGroup MyTargetGroup;
     private Transform myTransform;
 
     // Start is called before the first frame update
     void Start()
     {
+        float weight = normalWeight;
+
+        PlayerController playerController;
+        if(gameObject.TryGetComponent(out playerController))
+        {
+            if (PlayerBot.active && !PlayerBot.chosenPlayer.Contains(playerController.player))
+            {
+                weight = preferredWeight;
+            }
+        }
+
         myTransform = this.transform;
         MyTargetGroup = GameObject.Find("CAMERA/TargetGroup1").GetComponent<CinemachineTargetGroup>();
         if (!LobbyConnectionHandler.instance.IsMultiplayerMode)
         {
-            MyTargetGroup.AddMember(myTransform, 1.0f, 0f);
+            MyTargetGroup.AddMember(myTransform, weight, 0f);
         }
         else if(this.GetComponent<Photon.Pun.PhotonView>() && this.GetComponent<Photon.Pun.PhotonView>().IsMine)
         {
-            MyTargetGroup.AddMember(myTransform, 1.0f, 0f);
+            MyTargetGroup.AddMember(myTransform, weight, 0f);
         }
     }
 
